9b46bc7367e018baf103c0e7990afad86a7d74e2
ugur- <ugur-@SAMSUNG>
2025-01-08 23:32:55 +0300

    Add project files.

---
+++ SchoolManagementSystem/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem
+{
+    internal class Person
+    {
+        //field bölümünde değişkenlerimizi tanımlıyoruz ve private erişim belirleyicisi ile sadece bu classta erişim sağlıyoruz.(Encapsulation)
+        private string ad;
+        private string soyad;
+        private DateTime dogumTarihi;
+
+        //Propertyler ile bu değişkenlerimizi diğer claslardan okunabilir veya yazılabilir yapıyoruz. (Get, Set ile)
+        public string Ad
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+        public string Soyad
+        {
+            get { return soyad; }
+            set { soyad = value; }
+        }
+        public DateTime DogumTarihi
+        {
+            get { return dogumTarihi; }
+            set { dogumTarihi = value; }
+        }
+        //Ekrana yazmak için bir metot tanımlıyoruz.
+        public void ShowInformation()
+        {
+            Console.WriteLine($"{ad} {soyad} {dogumTarihi.ToString("dd.MM.yyyy")}");
+        }
+    }
+}
---
+++ SchoolManagementSystem/Program.cs
@@ -0,0 +1,38 @@
+﻿using SchoolManagementSystem;
+
+//Öğrenci 1 nesnesi oluşturuldu ve özellikleri tanımladı.
+Person student1 = new Person();
+student1.Ad = "Kamil";
+student1.Soyad = "ÖZTÜRK";
+student1.DogumTarihi = new DateTime(1999, 5, 15);
+
+//Öğrenci 2 nesnesi oluşturuldu ve özellikleri tanımladı.
+Person student2 = new Person();
+student2.Ad = "Ayşe";
+student2.Soyad = "GÜVEN";
+student2.DogumTarihi = new DateTime(1998, 2, 26);
+
+//Öğrenci 3 nesnesi oluşturuldu ve özellikleri tanımladı.
+Person student3 = new Person();
+student3.Ad = "Başak";
+student3.Soyad = "HASÇELİK";
+student3.DogumTarihi = new DateTime(1996, 1, 10);
+
+//Öğretmen 1 nesnesi oluşturuldu ve özellikleri tanımladı.
+Person teacher1 = new Person();
+teacher1.Ad = "Gizem";
+teacher1.Soyad = "ULU";
+teacher1.DogumTarihi = new DateTime(1991, 11, 7);
+
+//Öğretmen 2 nesnesi oluşturuldu ve özellikleri tanımladı.
+Person teacher2 = new Person();
+teacher2.Ad = "Mahmut";
+teacher2.Soyad = "ORHAN";
+teacher2.DogumTarihi = new DateTime(1992, 3, 7);
+
+// Öğrenci ve öğretmenler için Person sınıfının içerisindeki Show Information metodu çalıştırıldı.
+student1.ShowInformation();
+student2.ShowInformation();
+student3.ShowInformation();
+teacher1.ShowInformation();
+teacher2.ShowInformation();
